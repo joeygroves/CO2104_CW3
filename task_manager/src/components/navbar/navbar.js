9b8810719dd268e7c6410e2d8090310a02ed0259
Joey Groves <joeygroves@Joeys-MacBook-Pro.local>
2020-03-22 13:32:51 +0000

    Added nav bar

--- task_manager/src/components/navbar/navbar.js
+++ task_manager/src/components/navbar/navbar.js
@@ -1,1 +1,23 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./navbar.css";
+
+export default class NavBar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+                <div className="container">
+
+                    <Link className="navbar-brand" to={"/home"}>Task Management System</Link>
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link" to={"/logout"}>Logout</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
