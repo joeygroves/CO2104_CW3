988512a88dea495f54cbb98a9b322889cf748dd6
Joey Groves <joeygroves@Joeys-MacBook-Pro.local>
2020-03-22 13:33:32 +0000

    Added home page

--- task_manager/src/static/js/homePage.js
+++ task_manager/src/static/js/homePage.js
@@ -1,1 +1,19 @@
 import React, { Component } from "react";
+import "../css/homePage.css";
+import NavBar from "../../components/navbar/navbar";
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <NavBar />
+                </div>
+
+                <h3> Hello World! </h3>
+
+            </div>
+
+        );
+    }
+}
