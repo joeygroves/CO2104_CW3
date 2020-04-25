00881b147449c4cc6fdb698b2c2f147941ff1246
Joey Groves <joeygroves@joeys-mbp.home>
2020-04-25 22:59:33 +0100

    Updates

---
+++ precache-manifest.9fd63013777b995285921509914d04c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89cab0eb0001574f88c6c88da4425819",
+    "url": "/CO2104_CW3/index.html"
+  },
+  {
+    "revision": "b80104feda5d7a398995",
+    "url": "/CO2104_CW3/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c9970972d91429a3fe49",
+    "url": "/CO2104_CW3/static/css/main.b8729270.chunk.css"
+  },
+  {
+    "revision": "b80104feda5d7a398995",
+    "url": "/CO2104_CW3/static/js/2.8e76cdf1.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/CO2104_CW3/static/js/2.8e76cdf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9970972d91429a3fe49",
+    "url": "/CO2104_CW3/static/js/main.66239957.chunk.js"
+  },
+  {
+    "revision": "060c84ae8016077541e8",
+    "url": "/CO2104_CW3/static/js/runtime-main.963a2e4c.js"
+  }
+]);
